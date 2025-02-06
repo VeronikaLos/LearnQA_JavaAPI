0b170a8835469cd08df9d2c3e3ddaac318db2791
Veranika <losveronik@gmail.com>
2025-02-06 22:44:41 +0100

    testGetTest

---
+++ src/test/java/GetTextTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class GetTextTest {
+
+    @Test
+    public void testGetTest() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
