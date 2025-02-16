161cae3940dde4ba79a37d5c6df460037bc70efd
Veranika <losveronik@gmail.com>
2025-02-16 20:55:49 +0100

    Ex7: Долгий редирект

--- src/test/java/RedirectTest.java
+++ src/test/java/RedirectTest.java
@@ -18,4 +18,33 @@ public class RedirectTest {
         String locationHeader = response.getHeader("Location");
         System.out.println(locationHeader);
     }
+
+    @Test
+    public void testGetRestAssuredRedirect2() {
+        String url = "https://playground.learnqa.ru/api/long_redirect";
+        int redirectCount = 0;
+        final int maxRedirects = 10;
+
+        while (redirectCount < maxRedirects) {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(url)
+                    .andReturn();
+
+            int statusCode = response.getStatusCode();
+            if (statusCode == 200) {
+                System.out.println("Окончательный URL: " + url);
+                break;
+            }
+            url = response.getHeader("Location");
+            System.out.println(url);
+            redirectCount++;
+        }
+
+        System.out.println(redirectCount);
+    }
+
 }
