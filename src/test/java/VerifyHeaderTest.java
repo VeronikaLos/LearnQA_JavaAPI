d620669729db343eff58a5f7f2582c4ba802c289
Veranika <losveronik@gmail.com>
2025-03-02 14:05:44 +0100

    Ex12: Тест запроса на метод header

---
+++ src/test/java/VerifyHeaderTest.java
@@ -0,0 +1,22 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class VerifyHeaderTest {
+
+    @Test
+    public void testVerifyHeader() {
+        Response responseGetHeader = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+        Headers headers = responseGetHeader.getHeaders();
+        assertTrue (headers.hasHeaderWithName("x-secret-homework-header"), "Response doesn't have 'x-secret-homework-header' header");
+        String headerValue = headers.getValue("x-secret-homework-header");
+        assertEquals("Some secret value",headerValue);
+    }
+}
